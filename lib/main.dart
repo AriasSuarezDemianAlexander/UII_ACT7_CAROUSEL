bd9c0778b5fb2ca76bd5cb6b94b1e401bff31dc7
DEMIAN_ALEXANDER_ARIAS_SUAREZ <a.21308051280315@cbtis128.edu.mx>
2024-04-25 17:33:01 +0000

    aaa

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,98 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      debugShowCheckedModeBanner: false,
+      title: "Flutter Demo",
       theme: ThemeData(
-        // useMaterial3: false,
         primarySwatch: Colors.blue,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: "CARPINTERIA ARIAS"),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
   final String title;
-  const MyHomePage({super.key, required this.title});  
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageStae();
+}
+
+class _MyHomePageStae extends State<MyHomePage> {
+  final List<String> imgList = [
+    "https://thebelenfactory.com/img/Islas/isla1.jpeg",
+    "https://thebelenfactory.com/img/cocina/cocina.png",
+    "https://thebelenfactory.com/img/cocina/cocina2.jpeg",
+    "https://thebelenfactory.com/img/msd/msd.jpeg",
+    "https://thebelenfactory.com/img/Islas/isla4.jpeg",
+    "https://thebelenfactory.com/img/gabineteb/gb1.jpeg",
+  ];
+
+  int _currentPage = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: Text(widget.title),
+        backgroundColor: Colors.brown[200],
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      body: Padding(
+        padding: const EdgeInsets.only(
+            top: 10.0), // Ajusta el margen superior del carrusel
+        child: Column(
+          children: [
+            CarouselSlider(
+              items: imgList
+                  .map((e) => Center(
+                        child: Image.network(e),
+                      ))
+                  .toList(),
+              options: CarouselOptions(
+                  initialPage: 0,
+                  autoPlay: true,
+                  autoPlayInterval: const Duration(seconds: 2),
+                  enlargeCenterPage: true,
+                  enlargeFactor: 0.3,
+                  onPageChanged: (value, _) {
+                    setState(() {
+                      _currentPage == value;
+                    });
+                  }),
+            ),
+            buildCarouselIndicator()
+          ],
         ),
       ),
+    );
+  }
+
+  buildCarouselIndicator() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        for (int i = 0; i < imgList.length; i++)
+          Container(
+            margin: const EdgeInsets.all(5),
+            height: i == _currentPage ? 7 : 5,
+            width: i == _currentPage ? 7 : 5,
+            decoration: BoxDecoration(
+              color: i == _currentPage ? Colors.black : Colors.grey,
+              shape: BoxShape.circle,
+            ),
+          )
+      ],
     );
   }
 }
